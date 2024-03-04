dace81d9b3bab90b08f479c6f02289c9e8f42892
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-05 01:37:25 +0300

    Update paste.py

--- JoKeRUB/helpers/utils/paste.py
+++ JoKeRUB/helpers/utils/paste.py
@@ -5,7 +5,7 @@ import requests
 from ...Config import Config
 from ...core.logger import logging
 
-LOGS = logging.getLogger("JoKeRUB")
+LOGS = logging.getLogger("aljoker")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
@@ -70,6 +70,29 @@ async def s_paste(message, extension="txt"):
     return {"error": "Unable to reach spacebin."}
 
 
+def spaste(message, extension="txt"):
+    """
+    To Paste the given message/text/code to spaceb.in
+    """
+    siteurl = "https://spaceb.in/api/v1/documents/"
+    try:
+        response = requests.post(
+            siteurl, data={"content": message, "extension": extension}
+        )
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        if response["error"] != "" and response["status"] < 400:
+            return {"error": response["error"]}
+        return {
+            "url": f"https://spaceb.in/{response['payload']['id']}",
+            "raw": f"{siteurl}{response['payload']['id']}/raw",
+            "bin": "Spacebin",
+        }
+    return {"error": "Unable to reach spacebin."}
+
+
 async def n_paste(message, extension=None):
     """
     To Paste the given message/text/code to nekobin
@@ -99,7 +122,7 @@ async def d_paste(message, extension=None):
     """
     To Paste the given message/text/code to dogbin
     """
-    siteurl = "https://del.dog/documents"
+    siteurl = "http://catbin.up.railway.app/documents"
     data = {"content": message}
     try:
         response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
@@ -108,13 +131,13 @@ async def d_paste(message, extension=None):
     if response.ok:
         response = response.json()
         purl = (
-            f"https://del.dog/{response['key']}.{extension}"
+            f"http://catbin.up.railway.app/{response['key']}.{extension}"
             if extension
-            else f"https://del.dog/{response['key']}"
+            else f"http://catbin.up.railway.app/{response['key']}"
         )
         return {
             "url": purl,
-            "raw": f"https://del.dog/raw/{response['key']}",
+            "raw": f"http://catbin.up.railway.app/raw/{response['key']}",
             "bin": "Dog",
         }
     return {"error": "Unable to reach dogbin."}
