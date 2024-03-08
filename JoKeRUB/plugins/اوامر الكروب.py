1114fdb8795088ea5f0b8d04ef082cb3f407109d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-08 16:57:05 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -2,6 +2,7 @@
 from asyncio import sleep
 import asyncio
 import requests
+import random
 from datetime import datetime
 import time
 from telethon.tl import types
@@ -912,3 +913,49 @@ async def Husssein(event):
     global points
     points = {}
     await event.respond('**تم تصفير نقاط المشاركين بنجاح!**')
+
+trigger_symbol = ".محيبس"
+correct_answer = None
+game_board = "👊🏻👊🏻 👊🏻👊🏻 👊🏻👊🏻 👊🏻👊🏻\n1️⃣2️⃣ 3️⃣4️⃣ 5️⃣6️⃣ 7️⃣8️⃣"
+
+@l313l.on(events.NewMessage(incoming=True))
+async def handle_game_start(event):
+    global is_game_started2, correct_answer
+    if not is_game_started2 and trigger_symbol in event.raw_text:
+        is_game_started2 = True
+        correct_answer = random.randint(1, 8)
+        await event.edit(f"اين يوجد المحبس\n{game_board}\nيرجى اختيار الرقم الصحيح بين 1 و 8.")
+
+@l313l.on(events.NewMessage(outgoing=True, pattern=r'\.محيبس'))
+async def handle_clue(event):
+    global is_game_started2
+    if not is_game_started2:
+        return
+    await event.edit(f"اين يوجد المحبس\n{game_board}\nيرجى اختيار الرقم الصحيح بين 1 و 8.")
+
+@l313l.on(events.NewMessage(outgoing=True, pattern=r'\.طك (\d)'))
+async def handle_strike(event):
+    global is_game_started2, correct_answer, game_board
+    if is_game_started2:
+        strike_position = int(event.pattern_match.group(1))
+        if strike_position == correct_answer:
+            await event.respond("🎉 تهانينا! لقد وجدت المحبس!")
+        else:
+            position_index = (strike_position - 1) * 2
+            game_board = game_board[:position_index] + '🖐🏻' + game_board[position_index + 2:]
+            await event.respond(f"❌ للأسف، هذا ليس المحبس الصحيح.\n{game_board}")
+        is_game_started2 = False
+    else:
+        await event.respond("❌ لا يوجد لعبة قيد التشغيل.")
+
+@l313l.on(events.NewMessage(incoming=True))
+async def handle_guess(event):
+    global is_game_started2
+    if is_game_started2 and event.raw_text.isdigit():
+        guess = int(event.raw_text)
+        if 1 <= guess <= 8:
+            if guess == correct_answer:
+                await event.respond("🎉 تهانينا! لقد وجدت المحبس!")
+            else:
+                await event.respond("❌ للأسف، هذا ليس المحبس الصحيح.")
+            is_game_started2 = False
