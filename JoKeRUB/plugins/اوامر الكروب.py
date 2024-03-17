84cd9fdd37e564946a34fc04c873da8943c78c8d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-17 04:45:38 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -885,7 +885,7 @@ async def handle_start(event):
     global is_game_started, is_word_sent, word, bot_entity
     is_game_started = True
     is_word_sent = False
-    word = event.pattern_match.group(1)
+    word = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
     await event.edit(f"**اول من يكتب ( {word} ) سيفوز**")
 
 @l313l.on(events.NewMessage(incoming=True))
