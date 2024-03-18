b63479dc7d8d74360baa142465c7912b7b37281a
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-18 17:31:52 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -1001,8 +1001,7 @@ async def handle_incoming_message(event):
             'is_game_started2': False,
             'joker_player': None
         }
-    if group_game_status[chat_id]['is_game_started2']:
-        group_game_status[chat_id]['joker_player'] = event.sender_id
+    if group_game_status[chat_id]['is_game_started2'] and event.raw_text.lower() == "انا" and not group_game_status[chat_id]['joker_player']:
         await event.reply(f"**تم تسجيلك في المسابقة روح لحسين بظهرك\n{format_board(game_board, numbers_board)}**")
 def format_board(game_board, numbers_board):
     formatted_board = ""
