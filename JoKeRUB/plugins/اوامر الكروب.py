48ba4658013dbd6229ff4b29494f2ca3bb5ef48d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-28 08:52:05 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -99,9 +99,9 @@ async def handle_incoming_message(event):
     if sender_id in marriage:
         if event.text.lower() in ['نعم', 'لا']:
             if event.text.lower() == 'نعم':
-                aljoker_entity = await event.l313l.get_entity(replied_message.sender_id)
+                aljoker_entity = await event.client.get_entity(replied_message.sender_id)
                 aljoker_profile = f"[{aljoker_entity.first_name}](tg://user?id={aljoker_entity.id})"
-                replied_sender = await event.l313l.get_entity(event.sender_id)
+                replied_sender = await event.client.get_entity(event.sender_id)
                 replied_sender_profile = f"[{replied_sender.first_name}](tg://user?id={replied_sender.id})"
                 await event.reply(f'الف مبرووووك للزوجين {replied_sender_profile} و{aljoker_profile} الذي ارسلت نعم')
                 joker_marriage.append(sender_id)
