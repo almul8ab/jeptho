6cf7b40b93962ec0e91d463fe8b4b1a668de031c
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-28 06:51:06 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -74,13 +74,15 @@ async def handle_marriage_request(event):
 async def handle_reply(event):
     if event.reply_to_msg_id in marriage_requests:
         sender_id = event.sender_id
-        replied_to_user_id = await event.get_reply_message().from_id
-        if sender_id == replied_to_user_id:
-            if event.text.lower() == 'نعم':
-                await event.reply('الف مبروك لقد تم زواجك')
-            elif event.text.lower() == 'لا':
-                await event.reply('تم رفض طلب الزواج')
-            del marriage_requests[event.reply_to_msg_id]
+        replied_to_message = await event.get_reply_message()
+        if replied_to_message:
+            replied_to_user_id = replied_to_message.sender_id
+            if sender_id == replied_to_user_id:
+                if event.text.lower() == 'نعم':
+                    await event.reply('الف مبروك لقد تم زواجك')
+                elif event.text.lower() == 'لا':
+                    await event.reply('تم رفض طلب الزواج')
+                del marriage_requests[event.reply_to_msg_id]
 async def ban_user(chat_id, i, rights):
     try:
         await l313l(functions.channels.EditBannedRequest(chat_id, i, rights))
