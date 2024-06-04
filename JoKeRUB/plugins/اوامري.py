b590a8024a6678207938e11e124c290ed19fc5e4
ALAA <98186372+alaan1@users.noreply.github.com>
2024-06-05 02:00:18 +0300

    اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -43,7 +43,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                     Button.inline("( الصيغ ..) ⑥①", data="Contect"),
                     Button.inline(" ( التمبلر ..) ⑦①", data="Tumbler")],[Button.inline("(  الحساب ) ⑧①", data="Account")]
                     ,[Button.inline("( ميوزك ) ⑨①", data="Music"),Button.inline("( بصمات ) ⓪②", data="Voice")],[Button.inline("( التجميع ) ①②", data="Colect")]
-                    ,[Button.url("( اوامر البوت )", url="JEPTHON.t.me")]
+                    ,[Button.url("( قناة السورس )", url="JEPTHON.t.me")]
             ]
             if JEP_IC and JEP_IC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(
@@ -1465,7 +1465,7 @@ async def _(event):
                     Button.inline("( الصيغ ..) ⑥①", data="Contect"),
                     Button.inline(" ( التمبلر ..) ⑦①", data="Tumbler")],[Button.inline("(  الحساب ) ⑧①", data="Account")]
                     ,[Button.inline("( ميوزك ) ⑨①", data="Music"),Button.inline("( بصمات ) ⓪②", data="Voice")],[Button.inline("( التجميع ) ②①", data="Colect")]
-                    ,[Button.url("( اوامر البوت )", url="JEPTHON.t.me")]
+                    ,[Button.url("( قناة السورس )", url="JEPTHON.t.me")]
             ]
 	await event.edit(ROE, buttons=Home)
 
